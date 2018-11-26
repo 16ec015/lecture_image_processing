436096ccf447c293e04b8b5f2a0870b943cebf3a
ishiaki <16ec015@ms.dendai.ac.jp>
2018-11-26 15:48:31 +0900

    課題4

---
+++ 課題4/kadai4.m
@@ -0,0 +1,14 @@
+% 課題４　画像のヒストグラム
+% 画素の濃度ヒストグラムを生成せよ．
+% 下記はサンプルプログラムである．
+% 課題作成にあたっては「Lenna」以外の画像を用いよ．
+
+clear; % 変数のオールクリア
+
+ORG=imread('C:\Users\16ec015\Desktop\課題4\6d51cb36b9db550e57a21a26e688c434.jpg'); % 原画像の入力
+ORG=rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
+imagesc(ORG); colormap(gray); colorbar;
+pause;
+
+imhist(ORG);
+pause;% ヒストグラムの表示
